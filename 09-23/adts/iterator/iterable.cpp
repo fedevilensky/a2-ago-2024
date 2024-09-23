d0782092565dee443528d4cfba8f06b0b076f275
Fede Vilensky <fedevilensky@gmail.com>
2024-09-23 10:44:35 -0300

    bug fix

--- 09-23/adts/iterator/iterable.cpp
+++ 09-23/adts/iterator/iterable.cpp
@@ -1,14 +1,12 @@
 #pragma once
 
-template <class T>
-class iterator
-{
+template <class T> class iterator {
+public:
   virtual T next() = 0;
   virtual bool hasNext() = 0;
 };
 
-template <class T>
-class iterable
-{
+template <class T> class iterable {
+public:
   virtual iterator<T> *getIterator() = 0;
 };
